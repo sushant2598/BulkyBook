629b6a43b6410c938b0fb69d183f185b60bf7910
sushant.chahar <sushant.chahar@outlook.com>
2021-09-11 01:10:36 +0530

    Added CRUD operations

--- BulkyBook.DataAccess/Repository/CategoryRepository.cs
+++ BulkyBook.DataAccess/Repository/CategoryRepository.cs
@@ -24,7 +24,7 @@ namespace BulkyBook.DataAccess.Repository
             if (objFromDb != null)
             {
                 objFromDb.Name = category.Name;
-                _db.SaveChanges();
+                /*_db.SaveChanges();*/
             }
         }
     }
