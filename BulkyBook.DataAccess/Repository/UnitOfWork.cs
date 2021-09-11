54843f3362f32ecd3b1dd3053ac32feed0bd6dd6
sushant.chahar <sushant.chahar@outlook.com>
2021-09-11 16:43:26 +0530

    Added Company And User

--- BulkyBook.DataAccess/Repository/UnitOfWork.cs
+++ BulkyBook.DataAccess/Repository/UnitOfWork.cs
@@ -16,12 +16,18 @@ namespace BulkyBook.DataAccess.Repository
         {
             _db = db;
             Category = new CategoryRepository(_db);
+            Company = new CompanyRepository(_db);
             CoverType = new CoverTypeRepository(_db);
             Product = new ProductRepository(_db);
+            ApplicationUser = new ApplicationUserRepository(_db);
             SP_Call = new SP_Call(_db);
         }
 
+        public IApplicationUserRepository ApplicationUser { get; private set; }
+
         public ICategoryRepository Category { get; private set; }
+
+        public ICompanyRepository Company { get; private set; }
 
         public ICoverTypeRepository CoverType { get; private set; }
 
