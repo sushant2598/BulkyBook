645f0acac43c7cf8dabe848c23caf59c0316d58b
sushant.chahar <sushant.chahar@outlook.com>
2021-09-12 00:22:34 +0530

    Added All Models And Repositories

--- BulkyBook.DataAccess/Repository/UnitOfWork.cs
+++ BulkyBook.DataAccess/Repository/UnitOfWork.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using BulkyBook.DataAccess.Data;
+﻿using BulkyBook.DataAccess.Data;
 using BulkyBook.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace BulkyBook.DataAccess.Repository
 {
@@ -16,23 +14,24 @@ namespace BulkyBook.DataAccess.Repository
         {
             _db = db;
             Category = new CategoryRepository(_db);
-            Company = new CompanyRepository(_db);
             CoverType = new CoverTypeRepository(_db);
+            Company = new CompanyRepository(_db);
             Product = new ProductRepository(_db);
             ApplicationUser = new ApplicationUserRepository(_db);
             SP_Call = new SP_Call(_db);
+            OrderDetails = new OrderDetailsRepository(_db);
+            OrderHeader = new OrderHeaderRepository(_db);
+            ShoppingCart = new ShoppingCartRepository(_db);
         }
 
         public IApplicationUserRepository ApplicationUser { get; private set; }
-
         public ICategoryRepository Category { get; private set; }
-
         public ICompanyRepository Company { get; private set; }
-
-        public ICoverTypeRepository CoverType { get; private set; }
-
         public IProductRepository Product { get; private set; }
-
+        public ICoverTypeRepository CoverType { get; private set; }
+        public IShoppingCartRepository ShoppingCart { get; private set; }
+        public IOrderDetailsRepository OrderDetails { get; private set; }
+        public IOrderHeaderRepository OrderHeader { get; private set; }
         public ISP_Call SP_Call { get; private set; }
 
         public void Dispose()
